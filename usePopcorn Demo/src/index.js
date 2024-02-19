1ef1f616ffc57ed597fece4ef4686134dd5c3f92
Hristian Tuhlev <htuhlev88@gmail.com>
2024-02-19 14:41:56 +0200

    updating code

--- usePopcorn Demo/src/index.js
+++ usePopcorn Demo/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import StarRating from "./StarRating";
+import ToggleText from "./ShowMore";
 import "./index.css";
 import App from "./App";
 
@@ -7,5 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <ToggleText />
+    <StarRating
+      maxRating={5}
+      mesagges={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    /> */}
   </React.StrictMode>
 );
